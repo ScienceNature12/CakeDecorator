2f2ef5699321046436500e2dc1213ef458dd6a2b
ScienceNature <47803208+ScienceNature12@users.noreply.github.com>
2021-01-05 22:41:44 -0800

    Create TieredCake.java

---
+++ TieredCake.java
@@ -0,0 +1,19 @@
+//Author Tyler Bolen
+package cake_decorations;
+
+public class TieredCake extends MyCake
+{
+	public TieredCake()
+	{
+		description= "Tiered Cake: ";
+		
+	}//end TieredCake constructor 
+
+	@Override
+	public double cost()
+	{
+		return 20.00; 
+		
+	}//end cost
+
+}//end TieredCake class
