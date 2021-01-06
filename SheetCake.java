05b346ea568efaa428bf340f42b35c4d261e67ff
ScienceNature <47803208+ScienceNature12@users.noreply.github.com>
2021-01-05 22:41:17 -0800

    Create SheetCake.java

---
+++ SheetCake.java
@@ -0,0 +1,19 @@
+//Author Tyler Bolen
+package cake_decorations;
+
+public class SheetCake extends MyCake
+{
+	public SheetCake()
+	{
+		description= "Sheet Cake: ";
+		
+	}//end SheetCake constructor 
+
+	@Override
+	public double cost()
+	{
+		return 15.00; 
+		
+	}//end cost
+
+}//end SheetCake class
