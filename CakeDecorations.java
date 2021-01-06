e1c80dab948e31e40de6f5cda39e7d5e4c98802e
ScienceNature <47803208+ScienceNature12@users.noreply.github.com>
2021-01-05 22:38:24 -0800

    Create CakeDecorations.java

---
+++ CakeDecorations.java
@@ -0,0 +1,9 @@
+//Author Tyler Bolen
+package cake_decorations;
+
+public abstract class CakeDecorations extends MyCake
+{
+	
+	public abstract String  getDescription();
+	 	 
+}//end CakeDecorations class
